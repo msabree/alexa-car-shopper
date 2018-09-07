84e0cab9d0a9280635df8a9d3539569a363dcec1
Makeen Sabree <makeen.sabree@gmail.com>
2018-09-07 01:51:12 -0400

    Setting up scoring system.

--- helper/preferenceEngine.js
+++ helper/preferenceEngine.js
@@ -2,35 +2,108 @@
 /* eslint max-len: 0 */
 const includes = require('lodash/includes');
 const get = require('lodash/get');
+const meanBy = require('lodash/meanBy');
 
 // 'Machine Learning'
+const objDeviations = {
+    likes: {
+        miles: [],
+        year: [],
+        makeAndModel: {},
+        bodyStyle: {},
+        price: [],
+    },
+    dislikes: {
+        miles: [],
+        year: [],
+        makeAndModel: {},
+        bodyStyle: {},
+        price: [],
+    },
+};
 
-// Set default weights for all preferences that
-// we will analyze
-// Weights are between 1 - 5 in relation to importance
-// In the future we may let user configure these.
-// const objWeights = {
-//     mileage: 3,
-//     year: 3,
-//     makeAndModel: 3, // i think it makes sense to look at these together
-//     bodyStyle: 4,
-//     price: 5,
-//     color: 2,
-//     transmission: 5,
-// };
+const learn = (arrViewedCars, type) => {
+    for (let i = 0; i < arrViewedCars.length; i++) {
+        let miles = get(arrViewedCars[i], 'miles');
+        if (miles !== undefined) {
+            objDeviations[type].miles.push(miles);
+        }
 
-// test
-const computeScore = (objCarDetais) => {
-    if (objCarDetais.build.make.toLowerCase() === 'dodge') {
-        return 100;
-    } else if (objCarDetais.build.make.toLowerCase() === 'honda') {
-        return 90;
-    } else if (objCarDetais.build.make.toLowerCase() === 'ford') {
-        return 80;
-    } else if (objCarDetais.build.make.toLowerCase() === 'chevrolet') {
-        return 70;
+        let price = get(arrViewedCars[i], 'price');
+        if (price !== undefined) {
+            objDeviations[type].mileage.push(mileage);
+        }
+
+        let year = get(arrViewedCars[i], 'build.year');
+        if (year !== undefined) {
+            objDeviations[type].year.push(year);
+        }
+
+        let bodyStyle = get(arrViewedCars[i], 'body_type');
+        if (bodyStyle !== undefined) {
+            let currentCount = objDeviations[type].bodyStyle[body_type];
+            if (currentCount === undefined) {
+                objDeviations[type].bodyStyle[body_type] = 0;
+            } else {
+                objDeviations[type].bodyStyle[body_type] += 1;
+            }
+        }
+
+        let make = get(arrViewedCars[i], 'make');
+        let model = get(arrViewedCars[i], 'model');
+        if (make !== undefined && model != undefined) {
+            let currentCount = objDeviations[type].makeAndModel[`${make}|${model}`];
+            if (currentCount === undefined) {
+                objDeviations[type].makeAndModel[`${make}|${model}`] = 0;
+            } else {
+                objDeviations[type].makeAndModel[`${make}|${model}`] += 1;
+            }
+        }
     }
-    return 0;
+};
+
+// Compute mileage score
+const computeMileageScore = (car) => {
+    const currentCarMileage = get(car, 'miles');
+    console.log(`currentCarMileage ${currentCarMileage}`);
+    if (currentCarMileage === undefined) {
+        return 5;
+    }
+
+    // get like/dislike averages
+    const likeMilesAverage = meanBy(objDeviations.likes.miles, (intMiles) => {
+        return intMiles;
+    });
+
+    const dislikedMilesAverage = meanBy(objDeviations.dislikes.miles, (intMiles) => {
+        return intMiles;
+    });
+
+    if (dislikedMilesAverage > likeMilesAverage) {
+        // user wants a new car
+        if (currentCarMileage < dislikedMilesAverage && currentCarMileage < likeMilesAverage) {
+            return 10;
+        } else if (Math.abs(currentCarMileage - dislikedMilesAverage) > Math.abs(currentCarMileage - likeMilesAverage)) {
+            return 3;
+        } else if (Math.abs(currentCarMileage - dislikedMilesAverage) < Math.abs(currentCarMileage - likeMilesAverage)) {
+            return 7;
+        } else {
+            return 5;
+        }
+    } else if (dislikedMilesAverage < likeMilesAverage) {
+        // user wants an older car
+        if (currentCarMileage > dislikedMilesAverage && currentCarMileage > likeMilesAverage) {
+            return 10;
+        } else if (Math.abs(currentCarMileage - dislikedMilesAverage) < Math.abs(currentCarMileage - likeMilesAverage)) {
+            return 3;
+        } else if (Math.abs(currentCarMileage - dislikedMilesAverage) > Math.abs(currentCarMileage - likeMilesAverage)) {
+            return 7;
+        } else {
+            return 5;
+        }
+    } else {
+        return 5;
+    }
 };
 
 /**
@@ -45,6 +118,11 @@ const findTopResult = (arrResults, objStoredUserData) => {
     const alreadyShownIds = [];
     const likedCars = get(objStoredUserData, 'likes', []);
     const dislikedCars = get(objStoredUserData, 'dislikes', []);
+
+    learn(likedCars, 'likes');
+    learn(dislikedCars, 'dislikes');
+
+    console.log(objDeviations);
 
     for (let i = 0; i < likedCars.length; i++) {
         alreadyShownIds.push(likedCars[i].id);
@@ -62,7 +140,8 @@ const findTopResult = (arrResults, objStoredUserData) => {
     for (let i = 0; i < arrResults.length; i++) {
         // Dont show again
         if (!includes(alreadyShownIds, arrResults[i].id)) {
-            let currScore = computeScore(arrResults[i]);
+            let currScore = computeMileageScore(arrResults[i]);
+            console.log(`Curr Score (miles only) ${currScore}`);
             if (currScore > maxScore) {
                 maxScoreIndex = i;
                 maxScore = currScore;
