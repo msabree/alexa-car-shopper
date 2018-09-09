b83e655598ac510dc12e5ec1dab5444ba51ab01d
Makeen Sabree <makeen.sabree@gmail.com>
2018-09-09 11:29:53 -0400

    New helper to check for display support.

---
+++ helper/supportsDisplay.js
@@ -0,0 +1,9 @@
+module.exports = (handlerInput) => {
+    const hasDisplay =
+        handlerInput.requestEnvelope.context &&
+        handlerInput.requestEnvelope.context.System &&
+        handlerInput.requestEnvelope.context.System.device &&
+        handlerInput.requestEnvelope.context.System.device.supportedInterfaces &&
+        handlerInput.requestEnvelope.context.System.device.supportedInterfaces.Display;
+    return hasDisplay;
+};
